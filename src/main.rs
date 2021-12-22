e22f27e9100a8f03aa747e9ad816efb78500c28a
Ryoh Kawai <kawairyoh@gmail.com>
2021-12-23 07:19:02 +0900

    Add sample Namespace program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use std::ffi::CString;
+
+use anyhow::Result;
+use nix::sched::{unshare, CloneFlags};
+use nix::sys::wait::waitpid;
+use nix::unistd::{execv, fork, getpid, getuid, ForkResult};
+
+fn main() -> Result<()> {
+    // Start program
+    println!("Main PID: {}, User: {}", getpid(), getuid());
+
+    // unshare start
+    unshare(
+        CloneFlags::CLONE_FS
+            | CloneFlags::CLONE_NEWCGROUP
+            | CloneFlags::CLONE_NEWIPC
+            | CloneFlags::CLONE_NEWNET
+            | CloneFlags::CLONE_NEWNS
+            | CloneFlags::CLONE_NEWPID
+            | CloneFlags::CLONE_NEWUSER
+            | CloneFlags::CLONE_NEWUTS,
+    )
+    .expect("failed unshare");
+
+    println!("Under unshare PID: {} User: {}", getpid(), getuid());
+
+    // fork start (for run command)
+    match unsafe { fork().expect("failed fork") } {
+        ForkResult::Parent { child } => {
+            println!("I'm parent. PID: {} User: {}", getpid(), getuid());
+
+            // wait child process
+            waitpid(child, None).expect("failed waitpid");
+
+            println!("exit");
+        }
+        ForkResult::Child => {
+            println!("I'm child. PID: {} User: {}", getpid(), getuid());
+
+            // Run command
+            let path = CString::new("/bin/echo").expect("CString::new failed");
+            let argv =
+                ["echo", "Hello", "World"].map(|s| CString::new(s).expect("CString::new failed"));
+
+            execv(&path, &argv).expect("failed execv");
+        }
+    }
+
+    Ok(())
 }
